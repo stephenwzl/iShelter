35200344a5b511fd7729c163ab47aeb2e92c86ec
王志龙 <991405736qq@gmail.com>
2015-07-21 17:27:32 +0800

    todos

--- iShelter/CollectionViewController.h
+++ iShelter/CollectionViewController.h
@@ -5,6 +5,12 @@
 //  Created by wangzhilong on 15/7/20.
 //  Copyright (c) 2015年 wangzhilong. All rights reserved.
 //
+#pragma mark TODO
+/*
+ *完成退出时的位置记录
+ ＊完成书签
+ ＊完成收藏功能
+ */
 
 #import <UIKit/UIKit.h>
 
